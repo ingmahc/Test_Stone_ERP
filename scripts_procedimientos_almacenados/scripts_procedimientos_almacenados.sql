b34f08e16541e55937f00265f5c9d4165b9d6c3d
ingmahc <113489735+ingmahc@users.noreply.github.com>
2024-11-05 21:25:11 -0500

    Update scripts_procedimientos_almacenados.sql
    se realizo update para los procedimientos AssignRolePermission y AssignUserPermission

--- scripts_procedimientos_almacenados/scripts_procedimientos_almacenados.sql
+++ scripts_procedimientos_almacenados/scripts_procedimientos_almacenados.sql
@@ -17,7 +17,7 @@ BEGIN
     BEGIN TRY
         BEGIN TRANSACTION;
 
-        -- Validar si el rol existe
+        -- Verificar existencia del rol
         IF NOT EXISTS (SELECT 1 FROM Role WHERE id_role = @RoleId)
         BEGIN
             RAISERROR('El rol no existe.', 16, 1);
@@ -25,7 +25,7 @@ BEGIN
             RETURN;
         END
 
-        -- Validar si el permiso existe
+        -- Verificar existencia del permiso
         IF NOT EXISTS (SELECT 1 FROM Permission WHERE id_permi = @PermissionId)
         BEGIN
             RAISERROR('El permiso no existe.', 16, 1);
@@ -33,7 +33,7 @@ BEGIN
             RETURN;
         END
 
-        -- Validar si la entidad existe
+        -- Verificar existencia de la entidad
         IF NOT EXISTS (SELECT 1 FROM EntityCatalog WHERE id_entit = @EntityCatalogId)
         BEGIN
             RAISERROR('La entidad no existe.', 16, 1);
@@ -41,8 +41,28 @@ BEGIN
             RETURN;
         END
 
-        INSERT INTO PermiRole (role_id, permission_id, entitycatalog_id, perol_include)
-        VALUES (@RoleId, @PermissionId, @EntityCatalogId, @Include);
+        -- Insertar o actualizar permiso para el rol
+        IF NOT EXISTS (SELECT 1 FROM PermiRole WHERE role_id = @RoleId AND permission_id = @PermissionId AND entitycatalog_id = @EntityCatalogId)
+        BEGIN
+            INSERT INTO PermiRole (role_id, permission_id, entitycatalog_id, perol_include)
+            VALUES (@RoleId, @PermissionId, @EntityCatalogId, @Include);
+
+            DECLARE @RoleName NVARCHAR(255) = (SELECT role_name FROM Role WHERE id_role = @RoleId);
+            DECLARE @PermName NVARCHAR(255) = (SELECT name FROM Permission WHERE id_permi = @PermissionId);
+            DECLARE @EntityName NVARCHAR(255) = (SELECT entit_name FROM EntityCatalog WHERE id_entit = @EntityCatalogId);
+
+            DECLARE @Message NVARCHAR(400);
+            SET @Message = N'Permiso ''' + @PermName + ''' ' + CASE WHEN @Include = 1 THEN 'asignado' ELSE 'revocado' END + 
+                           ' para el rol ''' + @RoleName + ''' en la entidad ''' + @EntityName + '''.';
+
+            PRINT @Message;
+        END
+        ELSE
+        BEGIN
+            RAISERROR('El permiso ya está asignado al rol en la entidad especificada.', 16, 1);
+            ROLLBACK TRANSACTION;
+            RETURN;
+        END
 
         COMMIT TRANSACTION;
     END TRY
@@ -60,6 +80,9 @@ BEGIN
     END CATCH
 END
 GO
+
+
+
 /****** Object:  StoredProcedure [dbo].[AssignUserPermission]    Script Date: 3/11/2024 1:54:19 p. m. ******/
 SET ANSI_NULLS ON
 GO
@@ -69,7 +92,8 @@ CREATE PROCEDURE [dbo].[AssignUserPermission]
     @UserCompanyId BIGINT,
     @PermissionId BIGINT,
     @EntityCatalogId BIGINT,
-    @Include BIT
+    @Include BIT,
+    @Message NVARCHAR(4000) OUTPUT
 AS
 BEGIN
     SET NOCOUNT ON;
@@ -80,7 +104,7 @@ BEGIN
         -- Validar si el usuario existe
         IF NOT EXISTS (SELECT 1 FROM UserCompany WHERE company_id = @UserCompanyId)
         BEGIN
-            RAISERROR('El usuario no existe.', 16, 1);
+            SET @Message = 'El usuario especificado no existe.';
             ROLLBACK TRANSACTION;
             RETURN;
         END
@@ -88,7 +112,7 @@ BEGIN
         -- Validar si el permiso existe
         IF NOT EXISTS (SELECT 1 FROM Permission WHERE id_permi = @PermissionId)
         BEGIN
-            RAISERROR('El permiso no existe.', 16, 1);
+            SET @Message = 'El permiso especificado no existe.';
             ROLLBACK TRANSACTION;
             RETURN;
         END
@@ -96,13 +120,28 @@ BEGIN
         -- Validar si la entidad existe
         IF NOT EXISTS (SELECT 1 FROM EntityCatalog WHERE id_entit = @EntityCatalogId)
         BEGIN
-            RAISERROR('La entidad no existe.', 16, 1);
+            SET @Message = 'La entidad especificada no existe.';
             ROLLBACK TRANSACTION;
             RETURN;
         END
 
+        -- Obtener nombres descriptivos
+        DECLARE @CompanyName NVARCHAR(255), @PermissionName NVARCHAR(255), @EntityName NVARCHAR(255);
+        SELECT 
+			@CompanyName = B.compa_name 
+		FROM UserCompany A  
+		LEFT JOIN [dbo].[Company] B
+		ON A.company_id = B.id_compa
+		WHERE company_id = @UserCompanyId;
+        SELECT @PermissionName = name FROM Permission WHERE id_permi = @PermissionId;
+        SELECT @EntityName = entit_name FROM EntityCatalog WHERE id_entit = @EntityCatalogId;
+
+        -- Insertar permiso para el usuario
         INSERT INTO PermiUser (usercompany_id, permission_id, entitycatalog_id, peusr_include)
         VALUES (@UserCompanyId, @PermissionId, @EntityCatalogId, @Include);
+
+        -- Establecer mensaje de confirmación
+        SET @Message = CONCAT('Permiso "', @PermissionName, '" ha sido asignado al usuario "', @CompanyName, '" en la entidad "', @EntityName, '".');
 
         COMMIT TRANSACTION;
     END TRY
@@ -120,6 +159,9 @@ BEGIN
     END CATCH
 END
 GO
+
+
+
 /****** Object:  StoredProcedure [dbo].[GetRolePermissions]    Script Date: 3/11/2024 1:54:19 p. m. ******/
 SET ANSI_NULLS ON
 GO
